fb6f2213673977e593d814eb6bfb77486401b3d4
Jewelz <hujewelz@163.com>
2022-01-20 22:30:27 +0800

    formate date inside Text

---
+++ DateText/DateText/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  DateText
+//
+//  Created by luobobo on 2022/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Group {
+                Text(Date().formatted(date: .complete, time: .complete))
+                Text(Date().formatted(date: .long, time: .standard))
+                Text(Date().formatted(date: .numeric, time:  .shortened))
+                Text(Date().formatted(date: .abbreviated, time: .omitted))
+                Text(Date().formatted(date: .omitted, time: .complete))
+                Text(Date(), formatter: customFormatStyle)
+            }
+   
+            Group {
+
+            Text(Date(), style: .date)
+            Text(Date(), style: .time)
+
+            // 相对于当前时间的时间间隔，自动更新
+            Text(Date().addingTimeInterval(60 * 60), style: .relative)
+
+            // 定时器，自动更新
+            Text(Date().addingTimeInterval(60 * 60), style: .timer)
+            }
+            
+            Spacer()
+        }
+        .padding()
+            
+    }
+    
+    
+}
+
+private var customFormatStyle: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "EEE, dd MM"
+    return formatter
+}()
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DateText/DateText/DateTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateTextApp.swift
+//  DateText
+//
+//  Created by luobobo on 2022/1/20.
+//
+
+import SwiftUI
+
+@main
+struct DateTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
